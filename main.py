6512fdee61e33a9d0657b7c96f987633a8b2be20
boop-the-dog <aidenhperry@gmail.com>
2023-05-16 13:38:40 -0400

    added cool things

--- main.py
+++ main.py
@@ -7,23 +7,25 @@ intents = disnake.Intents.all()
 logs_channel = 1108080080711852042
 
 bot = commands.Bot(
-    command_prefix='?',
-    intents=intents,
-    test_guilds=[935560260725379143]
+    command_prefix="?", intents=intents, test_guilds=[935560260725379143]
 )
 
 # MESSAGE COMMANDS
 
+
 @bot.message_command(name="Send goofy ahh message")
-async def claim(inter:disnake.MessageCommandInteraction):
-    await inter.response.send_message("Notified <@" + str(inter.target.author.id) +">!", ephemeral= True)
-    await inter.target.reply(content ="imagine if this just works!")
+async def claim(inter: disnake.MessageCommandInteraction):
+    await inter.response.send_message(
+        "Notified <@" + str(inter.target.author.id) + ">!", ephemeral=True
+    )
+    await inter.target.reply(content="imagine if this just works!")
+
 
 # ON STARTUP
 @bot.event
 async def on_ready():
-    emb = disnake.Embed(color=disnake.Colour.green(),title=("**Bot started**"))
-    print(f'Logged in as {bot.user}')
+    emb = disnake.Embed(color=disnake.Colour.green(), title=("**Bot started**"))
+    print(f"Logged in as {bot.user}")
     channel = bot.get_channel(logs_channel)
     await channel.send(embed=emb)
 
