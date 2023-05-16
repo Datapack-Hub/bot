4e68a11e2231e50ffd7ba3d4d798668f5078efc2
Flynecraft <flynn.kresslein@gmx.de>
2023-05-16 20:33:18 +0200

    logging

--- main.py
+++ main.py
@@ -4,21 +4,45 @@ from bot_token import token
 
 intents = disnake.Intents.all()
 
-logs_channel = 1108080080711852042
-
 bot = commands.Bot(
-    command_prefix="?", intents=intents, test_guilds=[935560260725379143]
+    command_prefix="?", 
+    intents=intents, 
+    test_guilds=[935560260725379143]
 )
+
+logs_channel = 1108080080711852042
 
 # MESSAGE COMMANDS
 
-
-@bot.message_command(name="Send goofy ahh message")
+@bot.message_command(name="Redirect to Help Channel")
 async def claim(inter: disnake.MessageCommandInteraction):
-    await inter.response.send_message(
-        "Notified <@" + str(inter.target.author.id) + ">!", ephemeral=True
-    )
-    await inter.target.reply(content="imagine if this just works!")
+    redi_ban_role = bot.get_guild(935560260725379143).get_role(1108093399053111387)
+    if not redi_ban_role in inter.author.roles:
+        await inter.response.send_message(
+            "Notified <@" + str(inter.target.author.id) + ">! Abusing this command will result in punishment.", ephemeral=True
+        )
+        await inter.target.reply(content="imagine if this just works!")
+
+        # Logging
+
+        embed = disnake.Embed(color=disnake.Colour.orange(), 
+                            title=("**Redirect to Help Channel**"),
+                            description=("<@" + str(inter.user.id) + "> redirected a message by <@" + str(inter.target.author.id) +">! \nMessage Link: <#" + str(inter.channel.id) + ">")
+                            )
+        channel = bot.get_channel(logs_channel)
+        await channel.send(embed=embed)
+    else:
+        await inter.response.send_message(
+            "You are blacklisted from doing this. If you believe this is a miskate please contact a staff member.", ephemeral=True
+        )
+
+        # Logging
+        embed = disnake.Embed(color=disnake.Colour.orange(), 
+                            title=("**Redirect to Help Channel**"),
+                            description=("<@" + str(inter.user.id) + "> tried redirecting a message by <@" + str(inter.target.author.id) +">! \nMessage Link: <#" + str(inter.channel.id) + ">")
+                            )
+        channel = bot.get_channel(logs_channel)
+        await channel.send(embed=embed)
 
 
 # ON STARTUP
