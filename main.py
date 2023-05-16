e9259f51f044c18e2c1fb454f248d92d887800c7
Flynecraft <flynn.kresslein@gmx.de>
2023-05-16 22:08:54 +0200

    stuff

--- main.py
+++ main.py
@@ -21,16 +21,30 @@ async def claim(inter: disnake.MessageCommandInteraction):
         await inter.response.send_message(
             "Notified <@" + str(inter.target.author.id) + ">! Abusing this command will result in punishment.", ephemeral=True
         )
-        await inter.target.reply(content="imagine if this just works!")
+        embed = disnake.Embed(
+            color = disnake.Color.orange(), 
+            title="This question would be more fitting inside of a Help Channel!",
+            description="Help channels are our way of handeling datapack development related questions like yours, which is why you are recieving this message! Once you send a message/create a post there, every person in this server will be able to help you, including our experienced Helpers which you can ping (after some time of course, don't be impatient) if nobody else seems to know how to help you.\nIf you need help regarding Resource Packs, head to <#1051227454980755546> and if you are experiencing struggles developing your datapack <#1051225367807000706>! If you haven't already check out <#935570290317086841> which has some tips for asking your questions in the most efficent way!"
+                              )
+        
+        embed.set_author(
+            name = ("Requested by " + inter.author.name + "#" + inter.author.discriminator),
+            icon_url = inter.author.display_avatar
+        )
+
+        embed.set_footer(
+            text = "If you feel this function was used unfitting/inappropriate on purpose, please contact a staff member."
+        )
+        await inter.target.reply(embed=embed)
 
         # Logging
-
         embed = disnake.Embed(color=disnake.Colour.orange(), 
                             title=("**Redirect to Help Channel**"),
                             description=("<@" + str(inter.user.id) + "> redirected a message by <@" + str(inter.target.author.id) +">! \nMessage Link: <#" + str(inter.channel.id) + ">")
                             )
         channel = bot.get_channel(logs_channel)
         await channel.send(embed=embed)
+
     else:
         await inter.response.send_message(
             "You are blacklisted from doing this. If you believe this is a miskate please contact a staff member.", ephemeral=True
@@ -48,10 +62,10 @@ async def claim(inter: disnake.MessageCommandInteraction):
 # ON STARTUP
 @bot.event
 async def on_ready():
-    emb = disnake.Embed(color=disnake.Colour.green(), title=("**Bot started**"))
+    embed = disnake.Embed(color=disnake.Colour.green(), title=("**Bot started**"))
     print(f"Logged in as {bot.user}")
     channel = bot.get_channel(logs_channel)
-    await channel.send(embed=emb)
+    await channel.send(embed=embed)
 
 
 bot.run(token)
