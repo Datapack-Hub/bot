f0472bcd2a1b5d90fd93c5044ef9889beb3f77aa
Flynecraft <Flynecraft@users.noreply.github.com>
2023-07-27 09:55:44 +0000

    Apply formatting

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 methods = os.listdir(".\\method")
 
 for file in methods:
-    print("Method Name: " + file.replace(".txt",""))
+    print("Method Name: " + file.replace(".txt", ""))
     opened_file = open((".\\method\\" + file), "r")
     file_content = opened_file.read()
     print("Method Description: " + file_content)
