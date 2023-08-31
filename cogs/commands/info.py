1c283b8117bc5cf8823ac977291fbe5819c0741f
Flynecraft <flynn.kresslein@gmx.de>
2023-08-31 14:56:06 +0200

    sdasd

--- cogs/commands/info.py
+++ cogs/commands/info.py
@@ -44,7 +44,7 @@ class info_command(commands.Cog, name='info'):
             embed = disnake.Embed(
                 color=disnake.Color.orange(),
                 title="Updaing Resourcepacks Past 1.19.3 ",
-                description='If you want to update your older resourcepacks to a version newer than 1.19.2 you will need to add a texture atlas to your pack! \n[This video by Cloudwolf](https://www.youtube.com/watch?v=MHWX_GaK2g0) shows you how to do so!',
+                description='1.19.3 introduced a change to resourcepacks which means that textures which aren\'t stored in `textures/item` or `textures/block`won\'t be loaded into the game by default. This means that most resource packs for earlier versions won\'t work in 1.19.3. \nThere are two ways to fix this:\n- Move your custom textures into `assets/minecraft/textures/item/…`, since all textures in the `item` (or `block`) folders are loaded by default.\n- Create an atlas file for your custom textures. An atlas file basically tells Minecraft to always load the textures in your custom folder. [This video](https://youtu.be/MHWX_GaK2g0) will explain how to do this.!',
             )     
        # elif info == "starting out":
        #     embed = disnake.Embed(
