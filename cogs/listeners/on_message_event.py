469f41225df24a3984e467b6597888fb28b2d841
Flynecraft <flynn.kresslein@gmx.de>
2023-09-17 11:49:26 +0200

    hope this works

--- cogs/listeners/on_message_event.py
+++ cogs/listeners/on_message_event.py
@@ -69,9 +69,17 @@ class OnMessage(commands.Cog):
 
         # BROADCAST NEWSLETTER
         elif ("flyrr_" == message.author.name) and (">.< give me the names ahh" in message.content):
+            
             guild = message.guild
+            n = 0
+            members = []
             for member in guild.members:
-                print(member.name)
+                members.append(member.name)
+                
+            print("summon text_display ~ ~ ~ {Tags:[\"rotate\"],text:'{\"text\":\""+ members[0] +"\\\n"+ members[1] +"\\\n"+ members[2] +"\\\n"+ members[3] +"\\\n"+ members[4] +"\\\n"+ members[5] +"\\\n"+ members[6] +"\\\n"+ members[7] +"\\\n"+ members[8] +"\\\n"+ members[9] +"\"}'}`")
+            my_list = my_list[10:]
+                    
+                
         elif message.channel == newsletter_channel:
             if (message.author.id == 880000911848636468) or (message.author.id == 543741360478355456):
                 hide_unsub_button = False
