879942df96bcb5e99ce8b194ed59de242b7187ec
HoodieRocks <HoodieRocks@users.noreply.github.com>
2023-10-29 21:57:25 +0000

    Apply formatting

--- cogs/commands/eliminate.py
+++ cogs/commands/eliminate.py
@@ -11,9 +11,8 @@ class EliminateCommand(commands.Cog, name="eliminate"):
 
     @commands.slash_command(
         title="eliminate",
-        description="You did not see this. Nobody must know. Cover up your trace."
+        description="You did not see this. Nobody must know. Cover up your trace.",
     )
-    
     async def eliminate(
         self, inter: disnake.ApplicationCommandInteraction, target: disnake.User
     ):
@@ -29,9 +28,9 @@ class EliminateCommand(commands.Cog, name="eliminate"):
             for line in file_text:
                 line_2 = line
                 eliminated.append(line_2.replace("\n", ""))
-            
+
             print(eliminated)
-            
+
         with open(file_path_part) as file:
             file_text = file.readlines()
             participants = []
@@ -39,35 +38,37 @@ class EliminateCommand(commands.Cog, name="eliminate"):
             for line in file_text:
                 line_2 = line
                 participants.append(line_2.replace("\n", ""))
-            
-            print(participants)     
-                               
+
+            print(participants)
+
         if str(inter.user.id) in eliminated:
             embed = disnake.Embed(
                 color=disnake.Color.red(),
                 title="❓ Unknown Command",
-                description=("The command you've been searching for does not exist. Never existed. Never will exist.")
+                description=(
+                    "The command you've been searching for does not exist. Never existed. Never will exist."
+                ),
             )
-            
+
             error_code = randrange(300)
             if 0 <= error_code <= 99:
-                embed.set_footer(text="Error Code: 4C656176652E")  
+                embed.set_footer(text="Error Code: 4C656176652E")
             elif 100 <= error_code <= 199:
-                embed.set_footer(text="Error Code: 466F726765742E")  
+                embed.set_footer(text="Error Code: 466F726765742E")
             elif 200 <= error_code <= 299:
                 embed.set_footer(text="Error Code: 45726173652E")
             elif 200 <= error_code <= 300:
                 embed.set_footer(text="Err♡r C♡de: 4D69737320796F75203C33")
-                
+
             await inter.response.send_message(embed=embed, ephemeral=True)
-            
+
         else:
             if str(target) in eliminated:
                 with open(file_path, "a") as file:
                     file.write("\n" + str(inter.user.id))
                 if inter.user.id not in participants:
                     with open(file_path_part, "a") as file:
-                        file.write("\n" + str(inter.user.id))         
+                        file.write("\n" + str(inter.user.id))
                 embed = disnake.Embed(
                     color=disnake.Color.red(),
                     title="💥 Elimination Failed",
@@ -77,23 +78,22 @@ class EliminateCommand(commands.Cog, name="eliminate"):
                         + "> already is eliminated. You have been eliminated. 4."
                     ),
                 )
-                
+
             else:
                 with open(file_path, "a") as file:
                     file.write("\n" + str(target))
-                    
+
                 if inter.user.id not in participants:
                     with open(file_path_part, "a") as file:
-                        file.write("\n" + str(inter.user.id))  
+                        file.write("\n" + str(inter.user.id))
                 embed = disnake.Embed(
-                        color=disnake.Color.orange(),
-                        title="🧪 Elimination Successful",
-                        description=(
-                            "<@"
-                            + str(target)
-                            + "> has successfully been eliminated. Thank you for your loyality."
-                        ),
-                    )
-                
-            await inter.response.send_message(embed=embed)
+                    color=disnake.Color.orange(),
+                    title="🧪 Elimination Successful",
+                    description=(
+                        "<@"
+                        + str(target)
+                        + "> has successfully been eliminated. Thank you for your loyality."
+                    ),
+                )
 
+            await inter.response.send_message(embed=embed)
