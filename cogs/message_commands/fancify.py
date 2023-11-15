a1d8d20ec4b8ff55edf76815921b10199ffefe1c
Flynecraft <flynn.kresslein@gmx.de>
2023-11-15 22:37:22 +0100

    pucki i love you~

--- cogs/message_commands/fancify.py
+++ cogs/message_commands/fancify.py
@@ -13,18 +13,16 @@ class FancifyCommand(commands.Cog):
         if not message.embeds:
             if match:
                 embed = disnake.Embed(
-                    title=":sparkles:",
                     description="```hs\n" + match.group(0).strip().replace("`","") + "\n```",
                     color=disnake.Colour.purple()
-                ).set_footer(text="Since discord doesn't allow for the direct highlighting of mcfunction, this uses haskell to highlight text and might thus not be perfect\nRequested by " + inter.author.display_name,icon_url=inter.author.display_avatar.url)
+                )
 
                 
             else:
                 embed = disnake.Embed(
-                    title=":sparkles:",
                     description="```hs\n" + message.content + "\n```",
                     color=disnake.Colour.purple()
-                ).set_footer(text="Since discord doesn't allow for the direct highlighting of mcfunction, this uses haskell to highlight text and might thus not be perfect\nRequested by " + inter.author.display_name,icon_url=inter.author.display_avatar.url)      
+                )  
                 
             await inter.target.reply(embed=embed)
             await inter.response.send_message("Fancification successful :sparkles:", ephemeral=True)
