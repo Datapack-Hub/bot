17d4ca06c7acc0fe4b3cd929abf10f689bd66607
Flynecraft <flynn.kresslein@gmx.de>
2023-11-22 17:40:22 +0100

    fancify improvements

--- cogs/message_commands/fancify.py
+++ cogs/message_commands/fancify.py
@@ -3,23 +3,152 @@ from disnake.ext import commands
 import variables
 import re
 
+def fancify(command):
+    cmds_raw: list[str] = []
+    
+    combine = False;
+    for line in command:
+        if combine:
+            cmds_raw[-1] += line + '\n'
+        else:
+            cmds_raw.append(line + '\n')
+        combine = line.endswith('\\')
+    
+    cmds: list[dict[str,str]] = []
+    
+    for line in cmds_raw:
+        if line.startswith('#'):
+            cmds.append({
+                "text": line,
+                "type": "comment",
+            })
+        elif line.startswith('$'):
+            cmds.append({
+                "text": line,
+                "type": "macro"
+            })
+        elif line:
+            word = re.match(r'^\w* ?', line)
+            cmds.extend([{
+                "text": word[0],
+                "type": "command",
+            }, {
+                "text": line.removeprefix(word[0]),
+                "type": "",
+            }])
+        else:
+            cmds.append({
+                "text": line,
+                "type": "",
+            })
+    
+    
+    
+    def match(pattern: str, kind: str, line: str):
+        out = []
+        last = 0
+        for m in re.finditer(pattern, line):
+            out.extend([{
+                "text": line[last:m.span()[0]],
+                "type": "",
+            }, {
+                "text": m[0],
+                "type": kind,
+            }])
+            last = m.span()[1]
+        out.append({
+            "text": line[last:],
+            "type": "",
+        })
+        return out
+    
+    
+    def replace(pattern: str, kind: str, array: list[dict[str,str]]) -> list[dict[str,str]]:
+        out = []
+        for cmd in array:
+            if cmd['type'] == "":
+                out.extend(
+                    match(
+                        pattern,
+                        kind,
+                        cmd['text'],
+                    )
+                )
+            else:
+                out.append(cmd)
+        return out
+    
+    # ignore namespace in json {}
+    #
+    
+    final = replace(
+        r'\\',
+        "comment",
+        replace(
+            r'\d*\.?\d+',
+            "number",
+            replace(
+                r'@[aeprs]',
+                "selector",
+                replace(
+                    r'(?<=run) \w+|(?<=run \\\n)\n\s+\w+',
+                    "command",
+                    replace(
+                        r'([\w\d_\-]*:[\w\d_\-/]+)|([\w\d_\-]+:[\w\d_\-/]*)',
+                        "resource",
+                        replace(
+                            r'(?<!\\)(\\{2})*".*?(?<!\\)(\\{2})*"',
+                            "string",
+                            cmds,
+                        )
+                    )
+                )
+            )
+    )
+    )
+    
+    output_value = ""
+    for d in final:
+        match d['type']:
+            case "comment":
+                output_value += '\033[30m' + d['text']
+            case "command":
+                output_value += '\033[35m' + d['text']
+            case "macro":
+                output_value += '\033[31m' + d['text']
+            case "string":
+                output_value += '\033[34m' + d['text']
+            case "resource":
+                output_value += '\033[33m' + d['text']
+            case "number":
+                output_value += '\033[36m' + d['text']
+            case "selector":
+                output_value += '\033[36m' + d['text']
+            case _:
+                output_value += '\033[34m' + d['text']
+    return output_value
+         
 class FancifyCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.message_command(name="Fancify ✨")
     async def fancify(self, inter: disnake.MessageCommandInteraction,message):
-        match = re.search(r'\`\`\`([\s\S]*?)\`\`\`|\`([\s\S]*?)\`',message.content)
-        if not message.embeds:
-            if match:
+        description_real = ""
+        matches = re.findall(r'\`\`\`([\s\S]*?)\`\`\`|\`([\s\S]*?)\`',message.content)
+        for match in matches:
+            output = fancify(match.split('\n'))
+        if (not message.embeds) and (not message.stickers) and (not message.attachments):
+            if matches:
                 embed = disnake.Embed(
-                    description="```hs\n" + match.group(0).strip().replace("`","") + "\n```",
+                    description="```hs\n" + output + "\n```",
                     color=disnake.Colour.purple()
                 )
                 
             else:
+                output = fancify((message.content).split('\n'))
                 embed = disnake.Embed(
-                    description="```hs\n" + message.content + "\n```",
+                    description="```ansi\n" + output + "\n```",
                     color=disnake.Colour.purple()
                 )  
                 
@@ -27,6 +156,6 @@ class FancifyCommand(commands.Cog):
             await inter.response.send_message("Fancification successful :sparkles:", ephemeral=True)
 
         else:
-            await inter.response.send_message("You can't fancify embeds!", ephemeral=True)
+            await inter.response.send_message("You can't fancify this message!", ephemeral=True)
                 
             
