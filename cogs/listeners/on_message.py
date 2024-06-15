b0c9b1e3a8fa97149a7bf181109a77c07881d020
Flynecraft <flynn.kresslein@gmx.de>
2024-06-15 16:28:44 +0200

    stop stuff from breaking when using dev version of the bot

--- cogs/listeners/on_message.py
+++ cogs/listeners/on_message.py
@@ -66,10 +66,10 @@ class OnMessage(commands.Cog):
                 hooks = await message.channel.webhooks()
                 
                 for hook in hooks:
-                    if hook.name == "Datapack Helper Bot (Updated Webhook)":
+                    if hook.name == f"Datapack Helper Bot (Updated Webhook){variables.dev_bot_string}":
                         break
                 else:
-                    hook = await message.channel.create_webhook(name="Datapack Helper Bot (Updated Webhook)")
+                    hook = await message.channel.create_webhook(name=f"Datapack Helper Bot (Updated Webhook){variables.variables.dev_bot_string}")
                 
                 await message.delete()
                 await hook.send(replace_code_blocks(message.content),wait=False,username=message.author.display_name,avatar_url=message.author.display_avatar.url,allowed_mentions=disnake.AllowedMentions.none())
