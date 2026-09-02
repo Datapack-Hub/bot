e93ac41877a80a8f9b4fd2c52e45c40201ef37b6
Silabear <56885288+Silabear@users.noreply.github.com>
2026-09-02 12:47:52 +0100

    temporary bot command fix

--- command_data/dpwiki.py
+++ command_data/dpwiki.py
@@ -11,12 +11,12 @@ async def generate_dpwiki_data():
         return  # already populated
 
     logger = setup_logger("dpwiki_data")
-    async with aiohttp.ClientSession() as session, session.get("https://datapack.wiki/search.json") as req:
+    async with aiohttp.ClientSession() as session, session.get("https://wiki.datapackhub.net/search.json") as req:
         if req.status == 200:
             for page in await req.json():
                 out = {
                     "title": page["title"],
-                    "url": "https://datapack.wiki" + page["url"],
+                    "url": "https://wiki.datapackhub.net" + page["url"],
                     "description": page["description"],
                 }
 
