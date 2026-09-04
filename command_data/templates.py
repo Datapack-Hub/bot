1b0ff859e49b21f0203ece4f8dc7012f7b444784
Cobblestone <aidenhperry@gmail.com>
2026-09-04 13:21:13 -0400

    fix templates and add 26.2 templates

--- command_data/templates.py
+++ command_data/templates.py
@@ -1,12 +1,14 @@
 # Note: "versions" cannot have + in the name.
 
 DATAPACKS = [
+    {"versions": "26.2", "file": "dp-26.2+.zip"},
     {"versions": "26.1", "file": "dp-26.1+.zip"},
     {"versions": "1.21.11", "file": "dp-1.21.11+.zip"},
     {"versions": "1.21.4+", "file": "dp-1.21+.zip"},
 ]
 
 RESOURCEPACKS = [
+    {"versions": "26.2", "file": "rp-26.2+.zip"},
     {"versions": "26.1", "file": "rp-26.1+.zip"},
     {"versions": "1.21.11", "file": "rp-1.21.11+.zip"},
     {"versions": "1.21.4+", "file": "rp-1.21+.zip"},
